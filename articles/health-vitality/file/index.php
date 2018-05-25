c3e5c6f93a3b9a5628e2f9159c5ee4406df30101
Bryan Sng <sngby98@gmail.com>
2018-05-25 23:07:48 +0100

    Added articles

---
+++ articles/health-vitality/file/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+	<head>
+		{% include_relative resources.html %}
+		<title> - The Social</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	</head>
+	
+	<body>
+		<?php include (dirname(dirname(dirname(dirname(__FILE__)))).'/header.php'); ?>
+		
+		<!-- Intro from here
+		-->
+		<!-- Content from here
+		-->
+		<div class="content">
+			<h1>
+			</h1>
+			<p>
+			</p>
+		</div>
+		
+		<?php include (dirname(dirname((dirname(__FILE__)))).'/js/nav_tab_js.html'); ?>
+		
+		<?php include (dirname(dirname(dirname(dirname(__FILE__)))).'/header_js.php'); ?>
+		<?php include (dirname(dirname(dirname(dirname(__FILE__)))).'/footer.php'); ?>
+	</body>
+
+</html>
